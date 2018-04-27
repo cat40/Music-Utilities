0e05e488e31e8008dc11221ad3f5de3158ef7a87
cat40 <19844790+cat40@users.noreply.github.com>
2018-04-27 08:47:46 -0400

    Implimented use of lysrc.Pitch

--- lyutils/constants.py
+++ lyutils/constants.py
@@ -5,5 +5,6 @@ FLAT = 'b'
 VALIDACCIDENTALS = (LYFLAT, LYSHARP)
 VALIDNOTENAMESNOACCIDENTALS = list('abcdefg')
 VALIDNOTENAMES = tuple(' '.join(' '.join([s, s+LYFLAT, s+LYSHARP]) for s in VALIDNOTENAMESNOACCIDENTALS).split())
+STRNOTEDURATIONS = {'\\breve' : -1, '\\longa' : -2}
 VALIDNOTEDURATIONS = tuple(' '.join(' '.join([str(s), str(s)+'.', str(s)+'..']) for s in (2**x for x in range(7+1))).split()) + \
-                     tuple(' '.join(' '.join([s, s+'.', s+'..']) for s in ('\\longa', '\\breve')).split())
+                     tuple(' '.join(' '.join([s, s+'.', s+'..']) for s in STRNOTEDURATIONS.keys()).split())
--- lyutils/note.py
+++ lyutils/note.py
@@ -1,14 +1,8 @@
-from .constants import VALIDNOTENAMESNOACCIDENTALS, VALIDNOTEDURATIONS, SHARP, FLAT, LYSHARP, LYFLAT
-from .lysrc import Pitch
+from .constants import VALIDNOTEDURATIONS, STRNOTEDURATIONS
+from . import lysrc
 
-ACCIDENTALS = {SHARP : LYSHARP,
-               FLAT : LYFLAT,
-               LYSHARP : LYSHARP,
-               LYFLAT : LYFLAT}
 
-previous_pitch = None
-'''
-notes:
+'''notes:
 currently, a duration must be specified (unlike in lilypond, where a note automatically
 takes the duration of it's predecessor
 
@@ -20,158 +14,37 @@ accidental: Whether the note is sharp or flat. Will be constants.LYSHARP or cons
 
 '''
 class lyNote(object):
-    def __init__(self, string):
-        self.note = string[0]
-        if len(string) == 2:
-            self.duration = string[1]
-        if SHARP in string or LYSHARP in string:
-            self.accidental = LYSHARP
-        elif FLAT in string or LYFLAT in string:
-            self.accidental = LYFLAT
-        if "'" in string:
-            self.octave = "'" * string.count("'")
-        elif ',' in string:
-            self.octave = ',' * string.count(',')
+    def __init__(self, dur):
+        self.pitches = []
+        if isinstance(dur, Duration):
+            self.duration = dur
         else:
-            self.octave = ''
+            self.duration = Duration(dur)
 
-
-
-    @classmethod
-    def fromparts(cls, name, accidental, octave, length):
-        cls(name+ACCIDENTALS[accidental]+octave+length)
-        # self._name = name
-        # self._accidental = accidental
-        # self._length = length
-        # self._octave = octave
-
-
-    @property
-    def name(self):
-        return self._name
-
-    @name.setter
-    def name(self, value):
-        if value not in VALIDNOTENAMESNOACCIDENTALS:
-            raise ValueError('%s is not a valid note name')
-        self._name = value
-
-    @property
-    def duration(self):
-        return self._duration
-
-    @duration.setter
-    def duration(self, value):
-        if value not in VALIDNOTEDURATIONS:
-            raise ValueError('%s is not a valid not duration' % value)
-        self._duration = value
-
-    @property
-    def octave(self):
-        return self._octave
-
-    @octave.setter
-    def octave(self, value):
-        if not (len(value) == 0 or value == len(value) * "'" or value == len(value) * ","):
-            raise ValueError('%s is not a valid octave' % value)
-        self._octave = value
-
-    @property
-    def accidental(self):
-        return self._accidental
-
-    @accidental.setter
-    def accidental(self, value):
-        if value not in (LYFLAT, LYSHARP):
-            raise ValueError('%s is not a valid accidental' % value)
-        self._accidental = value
-
-
-'''
-From LilyPond 2.18.2 source code
-lilypond.org
-'''
-class Pitch:
-    def __init__ (self):
-        self.alteration = 0
-        self.step = 0
-        self.octave = 0
-        self._force_absolute_pitch = False
-
-    def __repr__(self):
-        return self.ly_expression()
-
-    def transposed (self, interval):
-        c = self.copy ()
-        c.alteration  += interval.alteration
-        c.step += interval.step
-        c.octave += interval.octave
-        c.normalize ()
-
-        target_st = self.semitones()  + interval.semitones()
-        c.alteration += target_st - c.semitones()
-        return c
-
-    def normalize (c):
-        while c.step < 0:
-            c.step += 7
-            c.octave -= 1
-        c.octave += c.step / 7
-        c.step = c.step  % 7
-
-    def lisp_expression (self):
-        return '(ly:make-pitch %d %d %d)' % (self.octave,
-                                             self.step,
-                                             self.alteration)
-
-    def copy (self):
-        p = Pitch ()
-        p.alteration = self.alteration
-        p.step = self.step
-        p.octave = self.octave
-        return p
-
-    def steps (self):
-        return self.step + self.octave*7
-
-    def semitones (self):
-        return self.octave * 12 + [0,2,4,5,7,9,11][self.step] + self.alteration
-
-    def ly_step_expression (self):
-        return pitch_generating_function (self)
-
-    def absolute_pitch (self):
-        if self.octave >= 0:
-            return "'" * (self.octave + 1)
-        elif self.octave < -1:
-            return "," * (-self.octave - 1)
+    def __str__(self):
+        if len(self.pitches) > 1:
+            string = '<' + ' '.join(self.pitches) + '>'
         else:
-            return ''
+            string = str(self.pitches)
+        return string + str(self.duration)
 
-    def relative_pitch (self):
-        global previous_pitch
-        if not previous_pitch:
-            previous_pitch = self
-            return self.absolute_pitch ()
-        previous_pitch_steps = previous_pitch.octave * 7 + previous_pitch.step
-        this_pitch_steps = self.octave * 7 + self.step
-        pitch_diff = (this_pitch_steps - previous_pitch_steps)
-        previous_pitch = self
-        if pitch_diff > 3:
-            return "'" * ((pitch_diff + 3) // 7)
-        elif pitch_diff < -3:
-            return "," * ((-pitch_diff + 3) // 7)
+
+class Duration(object): # a class to hold duration, and convert the weird ones like breve and longa to ints
+    def __init__(self, duration):
+        if duration not in VALIDNOTEDURATIONS:
+            raise ValueError('%s is not a valid not duration' % duration)
+        self.duration = duration
+
+    def __int__(self):
+        if self.duration in STRNOTEDURATIONS:
+            return STRNOTEDURATIONS[self.duration]
         else:
-            return ""
+            return int(self.duration)
 
-    def ly_expression (self, relative_pitches=True):
-        string = self.ly_step_expression()
-        if relative_pitches and not self._force_absolute_pitch:
-            string += self.relative_pitch()
-        else:
-            string += self.absolute_pitch()
+    def __str__(self):
+        return self.duration
 
-        return string
 
-    def print_ly (self, outputter):
-        outputter (self.ly_expression())
+class Pitch(lysrc.Pitch):
+    def __str__(self):
+        return repr(self)
