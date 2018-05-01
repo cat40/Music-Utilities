fc7b6ce14f662ec8a4a481532383d3e6516763a3
cat40 <19844790+cat40@users.noreply.github.com>
2018-05-01 12:07:10 -0400

    added tests

--- lyutils/__init__.py
+++ lyutils/__init__.py
@@ -1,5 +1,5 @@
 from .constants import *  # constants, might leave in constants later
-from .instrument import *  # instrument class and presets
+from .instrument import Instrument  # instrument class
 from .lymusic import Music  # music class
 from .lyObjs import *  # music components
 from .note import Pitch, Duration, Note  # note stuff
---
+++ tests.py
@@ -0,0 +1,17 @@
+import audioutils
+import os
+import lyutils # unused at the moment, but here to make sure the import works
+from audioutils import VIOLIN, VIOLA, CELLO
+
+TESTPATH = '.\\tests\\'
+RESULTSPATH = '.\\tests\\results\\'
+
+def test(fname, instruments):
+    test = audioutils.LibrosaObject(os.path.join(TESTPATH, fname))
+    test.getNotes()
+    test.outputNotes()
+    testmus = test.toMusic(instruments)
+    testmus.write(os.path.join(RESULTSPATH, fname+'.ly'))  # todo: remove previous file extension before the .ly
+
+
+instruments = [VIOLIN]
