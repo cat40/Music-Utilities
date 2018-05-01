0da9d59444680b00065d3232811903efed193af6
cat40 <19844790+cat40@users.noreply.github.com>
2018-05-01 13:42:09 -0400

    added modified lilypond source
    lilypond is distributed under the GNU liscense

---
+++ lyutils/lysrc/__init__.py
@@ -0,0 +1,7 @@
+'''
+All code in this module is directly from the LilyPond source, converted to Python 3
+LilyPond lives at lilypond.org
+'''
+print('in lysrc')
+from .musicexp import *
+print('musicexp')
