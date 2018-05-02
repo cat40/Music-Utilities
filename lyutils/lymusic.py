192ac12b144b390572828b4bfa433c07df8325df
cat40 <19844790+cat40@users.noreply.github.com>
2018-05-01 21:54:06 -0400

    fixed joining problem

--- lyutils/lymusic.py
+++ lyutils/lymusic.py
@@ -1,4 +1,4 @@
-from .lyObjs import *
+from .instrument import Instrument
 class Music(object):
     '''
     arguments:
@@ -6,12 +6,13 @@ class Music(object):
     \header{} thing
     @globals is a list of lyObj that go in the global section
     '''
-    def __init__(self, instruments, header=None, globalparts=None, midi=True):
+    def __init__(self, instruments, header=None, globalparts=None, midi=True, basenote=4):
         self.string = ''
         self.header = header if header is not None else {} # consider checking if the keys are valid, but that will be very hard to maintain as lilypond updates
                               # might actaully be able to import lilypond source code and use that to check if the keys are valid
         self.globalparts = globalparts if globalparts is not None else []
-        self.instruments = instruments
+        # if a global base is better than local ones, impliment it here
+        self.instruments = instruments if isinstance(instruments[0], Instrument) else [instrument.convert(basenote) for instrument in instruments]
 
     def __repr__(self):
         return 'Instrument object with %s instruments' % len(self.instruments) # todo: make this more descriptive
@@ -26,10 +27,10 @@ class Music(object):
         # globals
         if self.globalparts:
             string += '\\global = {\n'
-            string += '\n'.join(self.globalparts)
+            string += '\n'.join(map(str, self.globalparts))
             string += '\n}'
         # instruments
-        string += '\n\n'.join(self.instruments) + '\n'
+        string += '\n\n'.join(map(str, self.instruments)) + '\n'
         # instrument staff blocks
         string += '\n\n'.join(instrument.staffblock() for instrument in self.instruments)
         # score block
