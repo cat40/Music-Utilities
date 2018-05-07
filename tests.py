7f4ca4f7bddb3661642c1b4f8840a75e3ab49386
cat40 <19844790+cat40@users.noreply.github.com>
2018-05-07 13:19:12 -0400

    anothe test, sort of

--- tests.py
+++ tests.py
@@ -25,10 +25,10 @@ def testmidi():
     from mido import Message, MidiFile, MidiTrack
     mid = MidiFile()
     track = MidiTrack()
-    mid.tracks.append(track)
     track.append(Message('program_change', program=12, time=0))
     track.append(Message('note_on', note=64, velocity=64, time=32))
     track.append(Message('note_off', note=64, velocity=127, time=32))
+    mid.tracks.append(track)
     mid.save(os.path.join(RESULTSPATH, 'new_song.mid'))
 
 testmidi()
