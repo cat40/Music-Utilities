42373d0a762e1ff57fa1a397e2b8f42bbd9498e8
cat40 <19844790+cat40@users.noreply.github.com>
2018-05-10 12:41:20 -0400

    fixed script

--- midi2lyscript.bat
+++ midi2lyscript.bat
@@ -1,4 +1,5 @@
-set inname = %1
-set outname = %2
+@echo off
+set inname=%1
+set outname=%2
 
-"C:\Program Files (x86)\LilyPond\usr\bin\python2.4.exe" "C:\Program Files (x86)\LilyPond\usr\bin\midi2ly.py" %iname% -o %outname%
+"C:\Program Files (x86)\LilyPond\usr\bin\python2.4.exe" "C:\Program Files (x86)\LilyPond\usr\bin\midi2ly.py" %inname% -o %outname%
