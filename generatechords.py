81d980060779fc3c833a7076ede6ec6150857225
cat40 <19844790+cat40@users.noreply.github.com>
2018-05-16 09:18:10 -0400

    started random chord generation

--- generatechords.py
+++ generatechords.py
@@ -1,6 +1,8 @@
-import mingus
+import mingus.core.chords as Chords
+import mingus.core.progressions as Progressions
 import lyutils
 import librosa
+import random
 '''
 This file will generate chords, randomly or based on a specified progression
 '''
@@ -10,7 +12,7 @@ def genhelper(n, key, progression):
     :param n: Number of chords to generate. Need not be a multiple of the progression length
     :param key: The key the chords should be in
     :param progression:
-    :return: yields n Mingus chords
+    :return: yields n chord names (I, IV, etc)
     '''
     for i in range(n):
         if callable(progression):
@@ -18,6 +20,23 @@ def genhelper(n, key, progression):
         else:
             yield progression[len(progression) % i]
 
+def genChords(progressions):
+    '''
+    :param progressions: A list of the following strcuture:
+    [[chords, key, n=len(chords)]] or [[chordfunction, key, n]]
+    :return: All the chords as a list of Mingus chords
+    TODO implement octaves
+    '''
+    chords = []
+    for progression in progressions:
+        prog, key = progression[0:1+1]
+        try: n = progression[2]
+        except IndexError:
+            if not callable(prog):
+                n = len(prog)
+            else: raise
+        chords.append(Progressions.to_chords(genhelper(n, key, prog)))
+    return chords
 
 def toPitch(name, octave):
     '''
@@ -27,3 +46,20 @@ def toPitch(name, octave):
     '''
     return lyutils.Pitch.frommidi(librosa.core.note_to_midi(name+'0') + 12*octave)
 
+
+def random(_, key):
+    '''
+    :param _: garbage can for the index argument supplied in genhelper
+    :param key: the key the chord is in
+    :return: a random chord in the specified key
+    '''
+    pass
+
+
+def randomsimple(_, key):
+    '''
+    :param _: garbage can for the index argument supplied in genhelper
+    :param key: the key the chord is in
+    :return: a random chord in the specified key, using only the natural diatonic triads
+    '''
+    return lyutils.roman
