b2c2661b5adecf4e381b5150798d2519e00ff75b
cat40 <19844790+cat40@users.noreply.github.com>
2018-05-22 13:05:00 -0400

    added music play/pause

--- GUI.py
+++ GUI.py
@@ -9,7 +9,7 @@ Boxes for instruments, with some way to change the number (slider
 
 Song parameters:
     tempo
-    key
+    key (should be a slicder with the circle of fifths, and a checkbox for minor (or a menu for mode)
     time signature
 
 instrument parameters
@@ -18,9 +18,13 @@ instrument parameters
 
 todo:
 fixed values should grey out once the start button is pressed
+create a data structure to store the value of each button and the time it changed (or just the value of the buttons)
+    for every n frames of the audio
+make a class for instruments
 '''
 import time
 import tkinter as tk
+from pygame import mixer
 
 MAXFPS = 60
 PERIOD = 1/MAXFPS
@@ -36,17 +40,62 @@ def newScale(init=0, **kwargs):
     return scale
 
 
-numInstruments = newScale(init=1, orient=tk.HORIZONTAL, from_=1, to=15)  # cannot be changed during execution
-
-# window.mainloop()
-prevt = time.clock()
-while True:
-    if time.clock() - prevt < PERIOD:
-        continue
-    window.update()
-    print(numInstruments.get())
+def start():
+    # window.mainloop()
     prevt = time.clock()
+    while True:
+        if time.clock() - prevt < PERIOD:
+            continue
+        window.update()
+        print(numInstruments.get())
+        prevt = time.clock()
 
+
+def play():
+    prevPauseState = False
+    fname = fileBox.get()
+    music = mixer.Sound(fname)
+    music.play()
+    while True:
+        window.update()
+        # todo get values form all the buttons and thingies
+        if not mixer.get_busy():  # music is done playing
+            return  # todo put stuff into the data structure here
+        if prevPauseState != isPaused:
+            if isPaused:
+                mixer.pause()
+            else:
+                mixer.unpause()
+        prevPauseState = isPaused
+
+isPaused = False
+def pause():
+    global isPaused
+    isPaused = not isPaused
+
+
+mixer.init()
+# static values
+numInstruments = newScale(init=1, orient=tk.HORIZONTAL, from_=1, to=15)  # cannot be changed during execution
+fileBox = tk.Entry(window, text='File Name')
+fileBox.pack()
+
+startButton = tk.Button(window, text='Start', command=start)
+playButton = tk.Button(window, text='Play Music', command=play)
+pauseButton = tk.Button(window, text='Pause/Play Music', command=pause)
+startButton.pack()
+playButton.pack()
+pauseButton.pack()
+
+# dynamic values
+# global
+tempo = newScale(60, orient=tk.HORIZONTAL, from_=0, to=300, label='Tempo')
+key = tk.Spinbox(window, values=('C', 'D', 'E', 'F', 'G', 'A', 'B'), wrap=True)
+key.pack()
+
+
+while True:
+    window.update()
 
 '''
 references:
