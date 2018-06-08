346d43948214ee0d047f402157df10891012ecfe
cat40 <19844790+cat40@users.noreply.github.com>
2018-06-08 19:07:23 -0400

    added lower range bound slider and box

--- guihelper/instrument.py
+++ guihelper/instrument.py
@@ -1,6 +1,13 @@
 '''
 module for instrument-specific data
+
+instrument parameters
+    range (adustable during song to get better results)
+    how to get onsets (percussive/harmonic, what octaves to look in, etc)
+todo impliment soft and hard range bounds (right now all are hard)
 '''
+import tkinter as tk
+
 globalid = 0
 
 
@@ -11,3 +18,10 @@ class Instrument(object):
         globalid += 1
         self.window = window
         self.name = name
+        self.lowerRangeBound = tk.IntVar()  # midi number
+        self.lowerRangeSlider = tk.Scale(self.window, orient=tk.HORIZONTAL, from_=0, to=10,
+                                         label='Lower range bound', length=250, variable=self.lowerRangeBound)
+        self.lowerRangeBox = tk.Spinbox(self.window, variable=self.lowerRangeBound)
+
+    def displayInputs(self):
+        pass
