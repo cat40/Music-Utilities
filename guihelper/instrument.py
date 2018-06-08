c39b0a2ebfd0a558d52faea59812e579a56a9559
cat40 <19844790+cat40@users.noreply.github.com>
2018-06-08 19:13:39 -0400

    added upper range bound

--- guihelper/instrument.py
+++ guihelper/instrument.py
@@ -5,6 +5,7 @@ instrument parameters
     range (adustable during song to get better results)
     how to get onsets (percussive/harmonic, what octaves to look in, etc)
 todo impliment soft and hard range bounds (right now all are hard)
+todo make the note interface more friendly for use without familiarity with midi notes
 '''
 import tkinter as tk
 
@@ -19,9 +20,14 @@ class Instrument(object):
         self.window = window
         self.name = name
         self.lowerRangeBound = tk.IntVar()  # midi number
-        self.lowerRangeSlider = tk.Scale(self.window, orient=tk.HORIZONTAL, from_=0, to=10,
+        self.lowerRangeSlider = tk.Scale(self.window, orient=tk.HORIZONTAL, from_=0, to=127,
                                          label='Lower range bound', length=250, variable=self.lowerRangeBound)
         self.lowerRangeBox = tk.Spinbox(self.window, variable=self.lowerRangeBound)
+        self.upperRangeBound = tk.IntVar()  # midi number
+        self.upperRangeSlider = tk.Scale(self.window, orient=tk.HORIZONTAL, from_=0, to=127,
+                                         label='Lower range bound', length=250, variable=self.upperRangeBound)
+        self.upperRangeBox = tk.Spinbox(self.window, variable=self.upperRangeBound)
 
     def displayInputs(self):
-        pass
+        self.lowerRangeSlider.pack()
+        self.lowerRangeBox.pack()
