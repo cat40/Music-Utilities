7c4401865728c91166d06d2bcb4b931162641623
cat40 <19844790+cat40@users.noreply.github.com>
2018-06-08 20:32:55 -0400

    added all variables

--- guihelper/instrument.py
+++ guihelper/instrument.py
@@ -6,6 +6,7 @@ instrument parameters
     how to get onsets (percussive/harmonic, what octaves to look in, etc)
 todo impliment soft and hard range bounds (right now all are hard)
 todo make the note interface more friendly for use without familiarity with midi notes
+todo put the ranges on one scale instead of one for each bound
 '''
 import tkinter as tk
 
@@ -26,8 +27,25 @@ class Instrument(object):
         self.usePercussiveButton.select()
         self.useHarmonic = tk.BooleanVar()
         self.useHarmonicButton = tk.Checkbutton(self.window, text="Use Harmonic Onsets", variable=self.useHarmonic,
-                                                   onvalue=True, offvalue=False)
+                                                onvalue=True, offvalue=False)
         self.useHarmonicButton.select()
+        self.useOctaves = tk.BooleanVar()
+        self.useOctavesButton = tk.Checkbutton(self.window, text="Use Octave-splitting onset detection",
+                                               variable=self.useOctaves, onvalue=True, offvalue=False)
+        self.useOctavesButton.select()
+        self.octaveUpperBound = tk.IntVar()
+        self.octaveUpperBoundSlider = tk.Scale(self.window, orient=tk.HORIZONTAL, from_=0, to=8,
+                                               label='Upper Octave Bound', length=250, variable=self.octaveUpperBound)
+        self.octaveUpperBoundBox = tk.Spinbox(self.window, variable=self.octaveUpperBound)
+        self.octaveLowerBound = tk.IntVar()
+        self.octaveLowerBoundSlider = tk.Scale(self.window, orient=tk.HORIZONTAL, from_=0, to=8,
+                                               label='Lower Octave Bound', length=250, variable=self.octaveLowerBound)
+        self.octaveLowerBoundBox = tk.Spinbox(self.window, variable=self.octaveLowerBound)
+        self.staticVariables = [self.usePercussive, self.useHarmonic, self.useOctaves,
+                        self.octaveUpperBound, self.octaveLowerBound]
+        self.staticInputs = [self.usePercussiveButton, self.useOctavesButton, self.useHarmonicButton,
+                             self.octaveUpperBoundSlider, self.octaveUpperBoundBox,
+                             self.octaveLowerBoundSlider, self.octaveLowerBoundBox]
         # dynamic variables
         self.lowerRangeBound = tk.IntVar()  # midi number
         self.lowerRangeSlider = tk.Scale(self.window, orient=tk.HORIZONTAL, from_=0, to=127,
@@ -37,9 +55,36 @@ class Instrument(object):
         self.upperRangeSlider = tk.Scale(self.window, orient=tk.HORIZONTAL, from_=0, to=127,
                                          label='Lower range bound', length=250, variable=self.upperRangeBound)
         self.upperRangeBox = tk.Spinbox(self.window, variable=self.upperRangeBound)
+        self.dynamicVariables = [self.lowerRangeBound, self.upperRangeBound]
+        self.dynamicInputs = [self.lowerRangeBox, self.lowerRangeSlider, self.upperRangeSlider, self.upperRangeBox]
 
     def displayInputs(self):
+        # static variables
+        self.useHarmonicButton.pack()
+        self.usePercussiveButton.pack()
+        self.useOctavesButton.pack()
+        self.octaveLowerBoundBox.pack()
+        self.octaveLowerBoundSlider.pack()
+        self.octaveUpperBoundBox.pack()
+        self.octaveUpperBoundSlider.pack()
+        # dynamic variables
         self.lowerRangeSlider.pack()
         self.lowerRangeBox.pack()
         self.upperRangeBox.pack()
         self.upperRangeSlider.pack()
+
+    def readDynamicValues(self):
+        pass
+
+    def readStaticValues(self):
+        '''
+        reads the static values
+        Inputs are disabled once read
+        :return: None
+        '''
+        pass
+
+    def disableStaticVariables(self):
+        pass
+
+    def enableStaticVariables(self):
