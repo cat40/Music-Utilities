59ee0ef8cd5b1065ae01fa05d80ece370498ee85
cat40 <19844790+cat40@users.noreply.github.com>
2018-10-01 23:50:57 -0400

    added empty test_instrument file

---
+++ lyutils/tests/test_instrument.py
@@ -0,0 +1,18 @@
+from unittest import TestCase
+
+
+class TestInstrument(TestCase):
+    def test_from_raw_notes(self):
+        self.assertEquals()
+
+    def test_to_lilypond_string(self):
+        self.fail()
+
+    def test_staffblock(self):
+        self.fail()
+
+    def test_name(self):
+        self.fail()
+
+    def test_displayname(self):
+        self.fail()
