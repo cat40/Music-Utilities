17555b36672f264f200bbf998d2d331d82c17ae2
cat40 <19844790+cat40@users.noreply.github.com>
2018-10-06 20:23:31 -0400

    utilities.sum_stereo_to_mono should work with numpy arrays now

--- audioutils/utilities.py
+++ audioutils/utilities.py
@@ -7,21 +7,23 @@ GAIN_TO_CUT_IN_HALF = -6  # gain used to halve amplitude
 def sum_stereo_to_mono(left, right):
     '''
     take two waveforms and sum them together, accounting for duplication of center panned audio
+    todo: make a genric form that takes an arbitrary number of channels
     :param left: left channel waveform
     :param right: right channel waveform
     :return: merged waveform
     '''
-    # invert the right channel
-    right_inverted = right.invert_phase()
 
-    # combine right and left, to eliminat center
-    all_but_center = left.overlay(right_inverted).apply_gain(GAIN_TO_CUT_IN_HALF)
+    # invert the right channel
+    right_inverted = -1 * right
+
+    # combine right and left, to eliminate center
+    all_but_center = (left + right) * 0.5
 
     # halve the waveform of right and left channels to offset gain doubling of this algorithm
-    left_gain = left.apply_gain(GAIN_TO_CUT_IN_HALF)
-    right_gain = right.apply_gain(GAIN_TO_CUT_IN_HALF)
+    left_gain = left * 0.5
+    right_gain = right * 0.5
 
     # combine with original right and left channels
     # this will double everything but the center, to offset the center being doubled by the L+R sum
     # general volume should be twice the original volume
-    return left_gain.overlay(right_gain).overlay(all_but_center)
+    return left_gain + right_gain + all_but_center
